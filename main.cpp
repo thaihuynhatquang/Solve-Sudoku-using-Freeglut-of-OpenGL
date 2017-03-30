7ad9f75d5f5b3e5b1279e7ef23d1e4304fdfefce
Thái Huy Nhật Quang <thaihuynhatquang@gmail.com>
2017-03-31 01:22:12 +0700

    Fix bug

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <windows.h>
 #include <fstream>
 #include <GL/glut.h>
 #include <ctime>
@@ -7,36 +6,25 @@
 
 using namespace std;
 
-const int xMax = 9;
-const int xMin = 0;
-const int yMax = 9;
-const int yMin = 0;
-bool check_solve=false;
-bool check_press_number=false;
-bool check_clear_board=false;
-bool check_test=false;
-unsigned char s[9][9];
-unsigned char keyboardPress;
+const int xMax = 9, xMin = 0, yMax = 9, yMin = 0;
+bool check_solve=false, check_press_number=true, check_demo=true;
+unsigned char s[9][9], keyboardPress;
+int pos_click_x, pos_click_y, pos_press_x, pos_press_y;
 
-int pos_click_x;
-int pos_click_y;
-int pos_press_x;
-int pos_press_y;
-
+void init();
+void drawSquare(GLint x1, GLint y1, GLint x2, GLint y2, GLint x3, GLint y3, GLint x4, GLint y4);
+void keyboard(unsigned char key, int x, int y);
+void mouseClick(int button, int state, int x, int y);
+void drawBoard();
+void pressNumber();
+void clearBoard();
+void draw();
+void printText(int x, int y, char *str);
+void printText2(int x, int y, char *str);
+void printNumber(unsigned char s, GLint x, GLint y);
 void inputBoard();
 void solveBoard(int x, int y);
 bool checkBoard(int x, int y, int check);
-void init();
-void drawSquare(GLint x1, GLint y1, GLint x2, GLint y2, GLint x3, GLint y3, GLint x4, GLint y4);
-void draw();
-void drawBoard();
-void printNumber(unsigned char s, int x, int y);
-void keyboard(unsigned char key, int x, int y);
-void mouseClick(int button, int state, int x, int y);
-void mouseMotion(int x, int y);
-void printText(int x, int y, char *str);
-void printText2(int x, int y, char *str);
-void pressNumber();
 
 int main(int agrc, char ** argv)
 {
@@ -62,7 +50,7 @@ void init()
 void drawSquare(GLint x1, GLint y1, GLint x2, GLint y2, GLint x3, GLint y3, GLint x4, GLint y4)
 {
     glLineWidth(3);
-        glBegin(GL_LINE_LOOP);
+    glBegin(GL_LINE_LOOP);
         glVertex2i(x1, y1);
         glVertex2i(x2, y2);
         glVertex2i(x3, y3);
@@ -72,38 +60,7 @@ void drawSquare(GLint x1, GLint y1, GLint x2, GLint y2, GLint x3, GLint y3, GLin
 
 void keyboard(unsigned char key, int x, int y)
 {
-    switch (key)
-    {
-    case '1':
-        keyboardPress=key;
-        break;
-    case '2':
-        keyboardPress=key;
-        break;
-    case '3':
-        keyboardPress=key;
-        break;
-    case '4':
-        keyboardPress=key;
-        break;
-    case '5':
-        keyboardPress=key;
-        break;
-    case '6':
-        keyboardPress=key;
-        break;
-    case '7':
-        keyboardPress=key;
-        break;
-    case '8':
-        keyboardPress=key;
-        break;
-    case '9':
-        keyboardPress=key;
-        break;
-    default:
-        break;
-    }
+    keyboardPress=key;
 }
 
 void mouseClick(int button, int state, int x, int y)
@@ -133,34 +90,41 @@ void drawBoard()
     }
     glLineWidth(5);
     glPushAttrib(GL_CURRENT_BIT);
-    glColor3d(255, 255, 0);
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(765, 570);
-    glVertex2i(765, 470);
-    glVertex2i(600, 470);
-    glVertex2i(600, 570);
-    glEnd();
+        glColor3d(255, 255, 0);
 
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(765, 430);
-    glVertex2i(765, 330);
-    glVertex2i(600, 330);
-    glVertex2i(600, 430);
-    glEnd();
+        //draw square of "Demo"
+        glBegin(GL_LINE_LOOP);
+            glVertex2i(765, 570);
+            glVertex2i(765, 470);
+            glVertex2i(600, 470);
+            glVertex2i(600, 570);
+        glEnd();
 
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(765, 130);
-    glVertex2i(765, 30);
-    glVertex2i(600, 30);
-    glVertex2i(600, 130);
-    glEnd();
+        //draw square of "Press Number"
+        glBegin(GL_LINE_LOOP);
+            glVertex2i(765, 430);
+            glVertex2i(765, 330);
+            glVertex2i(600, 330);
+            glVertex2i(600, 430);
+        glEnd();
 
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(765, 280);
-    glVertex2i(765, 180);
-    glVertex2i(600, 180);
-    glVertex2i(600, 280);
-    glEnd();
+        //draw square of "Solve"
+        glBegin(GL_LINE_LOOP);
+            glVertex2i(765, 280);
+            glVertex2i(765, 180);
+            glVertex2i(600, 180);
+            glVertex2i(600, 280);
+        glEnd();
+
+        //draw square of "Clear Board"
+        glBegin(GL_LINE_LOOP);
+            glVertex2i(765, 130);
+            glVertex2i(765, 30);
+            glVertex2i(600, 30);
+            glVertex2i(600, 130);
+        glEnd();
+
+
     glPopAttrib();
 
     printText(645,515,"DEMO");
@@ -170,106 +134,107 @@ void drawBoard()
     printText(620,70,"Clear Board");
 
     glPushAttrib(GL_CURRENT_BIT);
-    glColor3d(255,255,0);
-    glLineWidth(10);
+        glColor3d(255,255,0);
+        glLineWidth(10);
 
-    glBegin(GL_LINES);
-        glVertex2i(210, 572);
-        glVertex2i(210, 29);
-    glEnd();
+        glBegin(GL_LINES);
+            glVertex2i(210, 572);
+            glVertex2i(210, 29);
+        glEnd();
 
-    glColor3d(255,255,0);
-    glLineWidth(10);
-    glBegin(GL_LINES);
-        glVertex2i(390, 572);
-        glVertex2i(390, 29);
-    glEnd();
+        glColor3d(255,255,0);
+        glLineWidth(10);
+        glBegin(GL_LINES);
+            glVertex2i(390, 572);
+            glVertex2i(390, 29);
+        glEnd();
 
-    glColor3d(255,255,0);
-    glLineWidth(10);
-    glBegin(GL_LINES);
-        glVertex2i(30, 390);
-        glVertex2i(570, 390);
-    glEnd();
+        glColor3d(255,255,0);
+        glLineWidth(10);
+        glBegin(GL_LINES);
+            glVertex2i(30, 390);
+            glVertex2i(570, 390);
+        glEnd();
 
-    glColor3d(255,255,0);
-    glLineWidth(10);
-    glBegin(GL_LINES);
-        glVertex2i(30, 210);
-        glVertex2i(570, 210);
-    glEnd();
+        glColor3d(255,255,0);
+        glLineWidth(10);
+        glBegin(GL_LINES);
+            glVertex2i(30, 210);
+            glVertex2i(570, 210);
+        glEnd();
 
-    glColor3d(255,255,0);
-    glLineWidth(10);
-    glBegin(GL_LINE_LOOP);
-        glVertex2i(30, 570);
-        glVertex2i(570, 570);
-        glVertex2i(570,30);
-        glVertex2i(30,30);
-    glEnd();
+        glColor3d(255,255,0);
+        glLineWidth(10);
+        glBegin(GL_LINE_LOOP);
+            glVertex2i(30, 570);
+            glVertex2i(570, 570);
+            glVertex2i(570,30);
+            glVertex2i(30,30);
+        glEnd();
     glPopAttrib();
 }
 
 void pressNumber()
 {
-    if(check_press_number==false)
+    if(check_press_number==true)
     {
         for(int i=0; i<xMax; i++)
             for(int j=0; j<yMax; j++)
                 s[i][j]='0';
-        check_clear_board=false;
     }
 }
 
 void clearBoard()
 {
-    if(check_clear_board==false)
-    {
-        for(int i=0; i<xMax; i++)
-            for(int j=0; j<yMax; j++)
-                s[i][j]='0';
-    }
+    for(int i=0; i<xMax; i++)
+        for(int j=0; j<yMax; j++)
+            s[i][j]='0';
 }
 
 void draw()
 {
+    drawBoard();
     //Click on "Demo"
-    if(pos_click_x>=600&&pos_click_x<=765&&pos_click_y<=560&&pos_click_y>=460)
+    if(pos_click_x>=600&&pos_click_x<=765&&pos_click_y<=570&&pos_click_y>=470)
     {
         inputBoard();
+        check_press_number=false;
+        check_solve=true;
     }
+
     //Click on "Solve"
-    if(pos_click_x>=600&&pos_click_x<=765&&pos_click_y<=270&&pos_click_y>=170)
+    if(pos_click_x>=600&&pos_click_x<=765&&pos_click_y<=280&&pos_click_y>=180&&check_solve==true)
     {
         solveBoard(0,0);
         check_press_number=false;
-        check_test=true;
-        check_clear_board=false;
+        check_demo=false;
     }
+
     //Click on "Press Number"
-    if(pos_click_x>=600&&pos_click_x<=765&&pos_click_y<=420&&pos_click_y>=320)
+    if(pos_click_x>=600&&pos_click_x<=765&&pos_click_y<=430&&pos_click_y>=330)
     {
+        check_solve=true;
         pressNumber();
-        check_clear_board=false;
-        check_solve=false;
-        check_test=false;
+        check_solve=true;
+        check_demo=false;
     }
+
     //Click on "Clear Board"
     if(pos_click_x>=600&&pos_click_x<=765&&pos_click_y<=130&&pos_click_y>=30)
     {
         clearBoard();
-        check_solve=false;
-        check_clear_board=true;
+        check_solve=true;
+        check_press_number=true;
     }
-    if(check_press_number==false&&check_test==false)
+
+    if(check_press_number==true&&30<=pos_click_x&&570>=pos_click_x&&30<=pos_click_y&&570>=pos_click_y)
     {
         int j = 8-(pos_click_x-30)/60;
         int i = (pos_click_y-30)/60;
         s[i][j]=keyboardPress;
-        cout << i << endl << j << endl << s[i][j];
     }
+
     GLint x, y;
-    drawBoard();
     for (y = 510; y >= 30; y -= 60)
     {
         for (x = 510; x >= 30; x -= 60)
@@ -277,9 +242,10 @@ void draw()
             if(s[y/60][8-x/60]!='0')
             {
                 glPushAttrib(GL_CURRENT_BIT);
-                glColor3f(255,255,0);
-                printNumber(s[y/60][8-x/60],x+25, y+20);
+                    glColor3f(255,255,0);
+                    printNumber(s[y/60][8-x/60],x+25, y+25);
                 glPopAttrib();
+
                 keyboardPress='0';
                 pos_click_x=0;
                 pos_click_y=0;
@@ -292,36 +258,36 @@ void draw()
 void printText(int x, int y, char *str)
 {
     glPushAttrib(GL_CURRENT_BIT);
-    glColor3b(229, 236, 234);
-    glRasterPos2f(x,y);
-    int n = (int) strlen(str);
-    for (int i = 0; i < n; i++)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,str[i]);
-    }
+        glColor3b(229, 236, 234);
+        glRasterPos2f(x,y);
+        int n = (int) strlen(str);
+        for (int i = 0; i < n; i++)
+        {
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,str[i]);
+        }
     glPopAttrib();
 }
 
 void printText2(int x, int y, char *str)
 {
     glPushAttrib(GL_CURRENT_BIT);
-    glColor3b(229, 236, 234);
-// Draw your text
-    glRasterPos2f(x,y);
-    int n = (int) strlen(str);
-    for (int i = 0; i < n; i++)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12,str[i]);
-    }
+        glColor3b(229, 236, 234);
+        // Draw your text
+        glRasterPos2f(x,y);
+        int n = (int) strlen(str);
+        for (int i = 0; i < n; i++)
+        {
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12,str[i]);
+        }
     glPopAttrib();
 }
 
-void printNumber(unsigned char s, int x, int y)
+void printNumber(unsigned char s, GLint x, GLint y)
 {
     glPushAttrib(GL_CURRENT_BIT);
-    glColor3b(229, 236, 234);
-    glRasterPos2d(x,y);
-    glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,s);
+        glColor3b(229, 236, 234);
+        glRasterPos2i(x,y);
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,s);
     glPopAttrib();
 }
 
@@ -335,86 +301,87 @@ void inputBoard()
         {
             ifstream file ("input1.txt");
             if(file.is_open())
-            while(!file.eof())
-            {
-            for(int i=xMax-1; i>=xMin; i--)
-                for(int j=yMax-1; j>=yMin; j--)
+                while(!file.eof())
                 {
-                    file >> s[i][j];
+                    for(int i=xMax-1; i>=xMin; i--)
+                        for(int j=yMax-1; j>=yMin; j--)
+                        {
+                            file >> s[i][j];
+                        }
                 }
-            }
             else cout << "Unable to open file";
             file.close();
             break;
         }
+
         case 2:
         {
             ifstream file ("input2.txt");
             if(file.is_open())
-            while(!file.eof())
-            {
-            for(int i=xMax-1; i>=xMin; i--)
-                for(int j=yMax-1; j>=yMin; j--)
+                while(!file.eof())
                 {
-                    file >> s[i][j];
+                    for(int i=xMax-1; i>=xMin; i--)
+                        for(int j=yMax-1; j>=yMin; j--)
+                        {
+                            file >> s[i][j];
+                        }
                 }
-            }
             else cout << "Unable to open file";
             file.close();
             break;
         }
+
         case 3:
         {
             ifstream file ("input3.txt");
             if(file.is_open())
-            while(!file.eof())
-            {
-            for(int i=xMax-1; i>=xMin; i--)
-                for(int j=yMax-1; j>=yMin; j--)
+                while(!file.eof())
                 {
-                    file >> s[i][j];
+                    for(int i=xMax-1; i>=xMin; i--)
+                        for(int j=yMax-1; j>=yMin; j--)
+                        {
+                            file >> s[i][j];
+                        }
                 }
-            }
             else cout << "Unable to open file";
             file.close();
             break;
         }
+
         case 4:
         {
             ifstream file ("input4.txt");
             if(file.is_open())
-            while(!file.eof())
-            {
-            for(int i=xMax-1; i>=xMin; i--)
-                for(int j=yMax-1; j>=yMin; j--)
+                while(!file.eof())
                 {
-                    file >> s[i][j];
+                    for(int i=xMax-1; i>=xMin; i--)
+                        for(int j=yMax-1; j>=yMin; j--)
+                        {
+                            file >> s[i][j];
+                        }
                 }
-            }
             else cout << "Unable to open file";
             file.close();
             break;
         }
+
         case 5:
         {
             ifstream file ("input5.txt");
             if(file.is_open())
-            while(!file.eof())
-            {
-            for(int i=xMax-1; i>=xMin; i--)
-                for(int j=yMax-1; j>=yMin; j--)
+                while(!file.eof())
                 {
-                    file >> s[i][j];
+                    for(int i=xMax-1; i>=xMin; i--)
+                        for(int j=yMax-1; j>=yMin; j--)
+                        {
+                            file >> s[i][j];
+                        }
                 }
-            }
             else cout << "Unable to open file";
             file.close();
             break;
         }
     }
-    check_clear_board=false;
-    check_press_number=false;
-    check_solve=false;
 }
 
 void solveBoard(int x, int y)
@@ -423,7 +390,7 @@ void solveBoard(int x, int y)
     {
         if(x==xMax-1)
         {
-            check_solve = true;
+            check_solve = false;
             return;
         }
         else solveBoard(x+1,yMin);
@@ -436,12 +403,13 @@ void solveBoard(int x, int y)
             {
                 s[x][y]=check+'0';
                 solveBoard(x,y+1);
-                if(check_solve==false)s[x][y]='0';
+                if(check_solve==true)s[x][y]='0';
             }
         }
     }
     else solveBoard(x,y+1);
 }
+
 bool checkBoard(int x, int y, int check)
 {
     char c_check=check+'0';
